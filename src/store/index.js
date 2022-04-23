0bdfccdaa8bdbc39f6d5b8f015afaa553a459e56
yoonhakeo <lani319@gmail.com>
2022-04-23 16:42:38 +0900

    vuex초기

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,25 @@
 import { createStore } from 'vuex'
+import { fetchNewsList } from '../api/index.js'
 
 export default createStore({
   state: {
-  },
-  getters: {
-  },
+    news: [],
+  },  
   mutations: {
+    SET_NEWS(state,news){
+        state.news = news;
+    }
   },
   actions: {
+    FETCH_NEWS(context){
+      fetchNewsList()
+      .then(response => {      
+        context.commit('SET_NEWS',response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
   },
   modules: {
   }
